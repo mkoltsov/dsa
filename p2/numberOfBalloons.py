a25614faed17e1d65a6233bd6f2b88929f510a68
mkoltsov <intimatik@gmail.com>
2025-12-09 00:53:43 -0500

    saved

--- p2/numberOfBalloons.py
+++ p2/numberOfBalloons.py
@@ -2,8 +2,24 @@ from collections import defaultdict
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         ch=defaultdict(str)
+        ballon=defaultdict(str)
         word="balloon"
         for c in text:
             ch[c] +=1
 
-        while     
+        for i in word:
+             ballon[i] +=1
+
+        matches=0
+        for k,v in ballon.items():
+            if k not in ch:
+                return 0
+            else:
+                if ch[k]==v:
+                    matches+=1
+        if matches==len(word):
+            return 1
+        else:
+            return 0            
+
+print(Solution().maxNumberOfBalloons("nlaebolko"))
